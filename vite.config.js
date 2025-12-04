9c2ea7a862edf4d80eb04c803dad07a2151d87e9
gauravv-g <gaurav.edi1@gmail.com>
2025-12-04 21:01:25 +0530

    Update and rename vite.config.ts to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({base: "/",}) => {
+export default defineConfig(({
+        plugins: [react()],
+        base: "/"
+    }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
